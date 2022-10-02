5b9318b8dfb90a615bb564618b37a937773806e4
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-02 19:13:47 +0300

    Backend <> Profile page: creating profile controller

--- dating-server/dating-server/routes/api.php
+++ dating-server/dating-server/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
