b660ee83041ab05b4aff9bb0b9be7529f06e5010
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-02 19:27:04 +0300

    Backend <> Landing route: adding possible routes for landing page

--- dating-server/dating-server/routes/api.php
+++ dating-server/dating-server/routes/api.php
@@ -8,16 +8,14 @@ use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ProfileController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(["prefix","v0.1"], function(){
+    Route::group(["prefix","landing"], function(){
+        Route::post("/signup", [LandingController::class, "signUp"])->name("landing-signup");
+        Route::post("/login", [LandingController::class, "logIn"])->name("landing-login"); 
+    });
+    
+    
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
