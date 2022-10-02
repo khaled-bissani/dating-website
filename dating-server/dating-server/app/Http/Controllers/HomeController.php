48db0710b6a6b96b7bfd93fb3678be3b675a48a1
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-02 20:05:15 +0300

    Backend <> Home page: setting up the functions

--- dating-server/dating-server/app/Http/Controllers/HomeController.php
+++ dating-server/dating-server/app/Http/Controllers/HomeController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    function viewPeople(){
+
+    }
+    function addFavorite(){
+
+    }
+    function deleteFavorite(){
+
+    }
+    function viewFavorite(){
+
+    }
+    function addBlock(){
+
+    }
+    function deleteBlock(){
+
+    }
 }
