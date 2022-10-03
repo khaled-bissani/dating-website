8741b23d137dd8bb115f57e68c4ca4f66300a9b6
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 21:28:12 +0300

    Backend <> Profile page: finishing edit profile api

--- dating-server/dating-server/app/Http/Controllers/ProfileController.php
+++ dating-server/dating-server/app/Http/Controllers/ProfileController.php
@@ -20,8 +20,34 @@ class ProfileController extends Controller
             "data" => $view_profile
         ]);
     }
-    function editProfile(){
-        
+    function editProfile(Request $request){
+        $user= User::all();
+        $id = $request->id;
+        $user = User::find($id);
+        $user->name = empty($request->name) ? $user->name : $request->name;
+        $user->email = empty($request->email) ? $user->email : $request->email;
+        $user->phone_number = empty($request->number) ? $user->phone_number : $request->number;
+        $user->age = empty($request->age) ? $user->age : $request->age;
+        $user->gender_interested = empty($request->gender_interested) ? $user->gender_interested : $request->gender_interested;
+        $user->interest = empty($request->interest) ? $user->interest : $request->interest;
+        $user->location = empty($request->location) ? $user->location : $request->location;
+        $user->bio = empty($request->bio) ? $user->bio : $request->bio;
+        $user->picture = empty($request->picture) ? $user->picture : $request->picture;
+
+        User::where('id',$id)->update([
+            'name' => $user->name,
+            'email' => $user->email,
+            'phone_number' => $user->phone_number,
+            'age' => $user->age,
+            'gender_interested' => $user->gender_interested,
+            'interest' => $user->interest,
+            'location' => $user->location,
+            'picture' => $user->picture,
+            'bio' => $user->bio
+        ]);
+        return response()->json([
+            "status" => "Success",
+        ]);
     }
     function visibile(){
         
