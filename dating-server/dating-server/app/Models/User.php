b005b8d0936003f199a3eb24f55b99403505ece8
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 15:24:57 +0300

    Backend <> User model: creating the user model with relations

--- dating-server/dating-server/app/Models/User.php
+++ dating-server/dating-server/app/Models/User.php
@@ -2,43 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function Favorite(){
+        return $this->belongsToMany(Favorite::class, 'favorites','users_id','users_id1');
+    }
+    public function Block(){
+        return $this->belongsToMany(Block::class, 'blocks','users_id','users_id1');
+    }
+    public function Chat(){
+        return $this->belongsToMany(Favorite::class, 'chats','users_id','users_id1');
+    }
+    public function Interest(){
+        return $this->belongsToMany(Favorite::class, 'interests','users_id','users_id1');
+    }
 }
