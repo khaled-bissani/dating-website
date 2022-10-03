149af3c64ecc52aca9384046af3b2bc9591daec0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 21:48:44 +0300

    Backend <> Chat page: finishing send chat api

--- dating-server/dating-server/app/Http/Controllers/ChatController.php
+++ dating-server/dating-server/app/Http/Controllers/ChatController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Chat;
 
 class ChatController extends Controller
 {
-    function sendChat(){
+    function sendChat(Request $request){
+        $message = $request->message;
+        $sender = $request->sender;
+        $receiver = $request->receiver;
 
+        Chat::insert([
+            'message' => $message,
+            'users_id' => $sender,
+            'users1_id' => $receiver
+        ]);
+        return response()->json([
+            "status" => "Success",
+        ]);
     }
     function receiveChat(){
         
