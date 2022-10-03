5e84b32eae97e6051b7fce74e1c4e5c2ef4be141
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 18:10:16 +0300

    Backend <> Middelware: installing jwt and configure it in the project

--- dating-server/dating-server/routes/api.php
+++ dating-server/dating-server/routes/api.php
@@ -9,31 +9,33 @@ use App\Http\Controllers\ProfileController;
 
 
 Route::group(["prefix"=>"v0.1"], function(){
-    Route::group(["prefix"=>"landing"], function(){
-        Route::post("/signup", [LandingController::class, "signUp"])->name("landing-signup");
-        Route::post("/continue_signup", [LandingController::class, "continueSignup"])->name("landing-continue-signup");
-        Route::post("/login", [LandingController::class, "logIn"])->name("landing-login"); 
-    });
-    Route::group(["prefix"=>"home"], function(){
-        Route::post("/view_people", [HomeController::class, "viewPeople"])->name("home-view-people");
-        Route::group(["prefix"=>"favorite"], function(){
-            Route::post("/add_favorite", [HomeController::class, "addFavorite"])->name("home-add-favorite");
-            Route::post("/delete_favorite", [HomeController::class, "deleteFavorite"])->name("home-delete-favorite"); 
-            Route::get("/view_favorite", [HomeController::class, "viewFavorite"])->name("home-view-favorite");
+    Route::group(["middleware" => "auth:api"], function(){
+        Route::group(["prefix"=>"landing"], function(){
+            Route::post("/signup", [LandingController::class, "signUp"])->name("landing-signup");
+            Route::post("/continue_signup", [LandingController::class, "continueSignup"])->name("landing-continue-signup");
+            Route::post("/login", [LandingController::class, "logIn"])->name("landing-login"); 
         });
-        Route::group(["prefix"=>"block"], function(){
-            Route::post("/add_block", [HomeController::class, "addBlock"])->name("home-add-block");
-            Route::post("/delete_block", [HomeController::class, "deleteBlock"])->name("home-delete-block"); 
+        Route::group(["prefix"=>"home"], function(){
+            Route::post("/view_people", [HomeController::class, "viewPeople"])->name("home-view-people");
+            Route::group(["prefix"=>"favorite"], function(){
+                Route::post("/add_favorite", [HomeController::class, "addFavorite"])->name("home-add-favorite");
+                Route::post("/delete_favorite", [HomeController::class, "deleteFavorite"])->name("home-delete-favorite"); 
+                Route::get("/view_favorite", [HomeController::class, "viewFavorite"])->name("home-view-favorite");
+            });
+            Route::group(["prefix"=>"block"], function(){
+                Route::post("/add_block", [HomeController::class, "addBlock"])->name("home-add-block");
+                Route::post("/delete_block", [HomeController::class, "deleteBlock"])->name("home-delete-block"); 
+            });
         });
-    });
-    Route::group(["prefix"=>"chat"], function(){
-        Route::post("/send_chat", [ChatController::class, "sendChat"])->name("send-chat");
-        Route::post("/receive_chat", [ChatController::class, "receiveChat"])->name("receive-chat");
-    });
-    Route::group(["prefix"=>"profile"], function(){
-        Route::post("/view_profile", [ProfileController::class, "viewProfile"])->name("view-profile");
-        Route::post("/edit_profile", [ProfileController::class, "editProfile"])->name("edit-profile");
-        Route::post("/visible", [ProfileController::class, "visibile"])->name("visible-profile");
+        Route::group(["prefix"=>"chat"], function(){
+            Route::post("/send_chat", [ChatController::class, "sendChat"])->name("send-chat");
+            Route::post("/receive_chat", [ChatController::class, "receiveChat"])->name("receive-chat");
+        });
+        Route::group(["prefix"=>"profile"], function(){
+            Route::post("/view_profile", [ProfileController::class, "viewProfile"])->name("view-profile");
+            Route::post("/edit_profile", [ProfileController::class, "editProfile"])->name("edit-profile");
+            Route::post("/visible", [ProfileController::class, "visibile"])->name("visible-profile");
+        });
     });
 });
 
