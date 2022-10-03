d3f7f374edbbdd95f1fc218f837614ba7d54f7c9
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 16:10:30 +0300

    Backend <> Landing page: finishing signup api

--- dating-server/dating-server/app/Http/Controllers/LandingController.php
+++ dating-server/dating-server/app/Http/Controllers/LandingController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LandingController extends Controller
 {
-    function signUp(){
+    function signUp(Request $request){
+        $name = $request->name;
+        $email = $request->email;
+        $number = $request->phone_number;
+        $age = $request->age;
+        $gender_interested= $request->gender_interested;
+        $gender = $request->gender;
 
+        User::insert([
+            'name' => $name,
+            'email' => $email,
+            'phone_number' => $number,
+            'age' => $age,
+            'gender_interested' => $gender_interested,
+            'genders_id' => $gender
+        ]);
+        return response()->json([
+            "status" => "Success",
+        ]);
     }
     function logIn(){
 
