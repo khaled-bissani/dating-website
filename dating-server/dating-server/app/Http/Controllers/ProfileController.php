dc594011971f3be281b79ee23a9eb51fb9462d26
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-03 20:31:39 +0300

    Backend <> Profile page: finishing view profile api

--- dating-server/dating-server/app/Http/Controllers/ProfileController.php
+++ dating-server/dating-server/app/Http/Controllers/ProfileController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
-    function viewProfile(){
+    function viewProfile(Request $request){
+        $id = $request->id;
 
+        $view_profile = User::
+        select('name','email','phone_number','age','picture','bio','interest','location','gender_interested')
+        ->where('id',$id)
+        ->get();
+
+        return response()->json([
+            "status" => "Success",
+            "data" => $view_profile
+        ]);
     }
     function editProfile(){
         
