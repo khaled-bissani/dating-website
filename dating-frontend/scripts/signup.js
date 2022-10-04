78fe6d33a6c5baaf2c936a4e855cd0a273900aff
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-04 09:04:48 +0300

    Frontend <> Landing page: adding validation for the input fields

--- dating-frontend/scripts/signup.js
+++ dating-frontend/scripts/signup.js
@@ -3,12 +3,12 @@ const continueButton = document.getElementById('continue-btn');
 const submitButton = document.getElementById('submit-btn');
 
 // Declare all input fields and drop down menu
-const fullName = document.getElementById('name').value;
-const email = document.getElementById('email').value; 
-const phoneNumber = document.getElementById('phone-number').value; 
-const age = document.getElementById('age').value; 
-const gender = document.getElementById('gender').value; 
-const genderInterested = document.getElementById('gender-interested').value;  
+const fullName = document.getElementById('name');
+const email = document.getElementById('email'); 
+const phoneNumber = document.getElementById('phone-number'); 
+const age = document.getElementById('age'); 
+const gender = document.getElementById('gender'); 
+const genderInterested = document.getElementById('gender-interested');  
 
 const baseLandingURL = "http://127.0.0.1:8000/api/v0.1/landing/";
 
@@ -34,7 +34,35 @@ const postAPI = async (api_url, api_data, api_token = null) => {
 // }
 
 const continueSignup = () => {
-
+    // regex to check email format
+    const emailFormat=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (fullName.value == '') {
+        fullName.style.borderColor = "red"; 
+        fullName.style.borderWidth = "2px";
+        fullName.placeholder = "No name";
+    }
+    if (phoneNumber.value == '') {
+        phoneNumber.style.borderColor = "red"; 
+        phoneNumber.style.borderWidth = "2px";
+        phoneNumber.placeholder = "No number";
+    }
+    if (age.value == '') {
+        age.style.borderColor = "red"; 
+        age.style.borderWidth = "2px";
+        age.placeholder = "No age";
+    }
+    if (email.value == '') {
+        email.style.borderColor = "red"; 
+        email.style.borderWidth = "2px";
+        email.placeholder = "No email";
+    }
+    else if(!emailFormat.test(email.value)){
+        email.style.borderColor = "red"; 
+        email.style.borderWidth = "2px";
+        email.value = '';
+        email.placeholder = "Not valid email";
+    }
 }
 
 continueButton.addEventListener('click',continueSignup)
