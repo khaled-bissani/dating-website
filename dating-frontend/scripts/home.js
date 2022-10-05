2ae9c9b9521d43c59c41753b99902697a66cdeb0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-05 14:40:29 +0300

    Frontend <> Home page: adding profile section

--- dating-frontend/scripts/home.js
+++ dating-frontend/scripts/home.js
@@ -1,12 +1,21 @@
 const homeSection = document.getElementById('home');
 const favoriteSection = document.getElementById('favorite');
+const profileSection = document.getElementById('profile');
 
 const goToFavorite =document.getElementById('go-to-favorite');
+const goToProfile =document.getElementById('go-to-profile');
 const logout =document.getElementById('logout');
 
 const toFavorite = () => {
   homeSection.style.display="none";
-  favoriteSection.style.display="block"
+  favoriteSection.style.display="block";
+  profileSection.style.display="none";
+}
+
+const toProfile = () => {
+  profileSection.style.display="block";
+  homeSection.style.display="none";
+  favoriteSection.style.display="none";
 }
 
 const logoutToLanding = () => {
@@ -15,3 +24,4 @@ const logoutToLanding = () => {
 
 logout.addEventListener('click',logoutToLanding);
 goToFavorite.addEventListener('click',toFavorite);
+goToProfile.addEventListener('click',toProfile);
