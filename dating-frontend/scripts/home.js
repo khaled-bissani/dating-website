1c24615ac083b3de48594e6449945086f0926165
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-05 19:57:40 +0300

    Frontend <> Home page: adding chat section

--- dating-frontend/scripts/home.js
+++ dating-frontend/scripts/home.js
@@ -1,13 +1,23 @@
 const homeSection = document.getElementById('home');
+const chatSection = document.getElementById('chat');
 const favoriteSection = document.getElementById('favorite');
 const profileSection = document.getElementById('profile');
 
+const goToChat =document.getElementById('go-to-chat');
 const goToFavorite =document.getElementById('go-to-favorite');
 const goToProfile =document.getElementById('go-to-profile');
 const logout =document.getElementById('logout');
 
+const toChat = () => {
+  chatSection.style.display="block";
+  homeSection.style.display="none";
+  favoriteSection.style.display="none";
+  profileSection.style.display="none";
+}
+
 const toFavorite = () => {
   homeSection.style.display="none";
+  chatSection.style.display="none";
   favoriteSection.style.display="block";
   profileSection.style.display="none";
 }
@@ -15,6 +25,7 @@ const toFavorite = () => {
 const toProfile = () => {
   profileSection.style.display="block";
   homeSection.style.display="none";
+  chatSection.style.display="none";
   favoriteSection.style.display="none";
 }
 
@@ -22,6 +33,7 @@ const logoutToLanding = () => {
   window.location.replace('landing.html');
 }
 
+goToChat.addEventListener('click',toChat);
 logout.addEventListener('click',logoutToLanding);
 goToFavorite.addEventListener('click',toFavorite);
 goToProfile.addEventListener('click',toProfile);
