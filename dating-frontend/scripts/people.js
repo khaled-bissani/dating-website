38904f468e4b1e054f9b0a752c5e6ccf3e742c29
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-05 14:17:53 +0300

    Frontend <> Home page: linking the view favorite api with the favorite section

--- dating-frontend/scripts/people.js
+++ dating-frontend/scripts/people.js
@@ -1,4 +1,5 @@
 const homeContainer = document.querySelector('.home-container');
+const favoriteContainer = document.querySelector('.favorite-container');
 const homeBaseURL = "http://127.0.0.1:8000/api/v0.1/home/";
 
 const postAPI = async (api_url, api_data = null, api_token = null) => {
@@ -18,6 +19,7 @@ const postAPI = async (api_url, api_data = null, api_token = null) => {
 
 window.onload = async() =>{
 
+    // Retreiving all the people from the database
     const view_people= new FormData();
     view_people.append('id', localStorage.getItem('currentUserId'));
     view_people.append('gender_interested',localStorage.getItem('genderIntersetedIn'));
@@ -55,8 +57,44 @@ window.onload = async() =>{
         `
     }
 
+    // Retreiving all the favorite from the database
+    const view_favorite= new FormData();
+    view_favorite.append('id', localStorage.getItem('currentUserId'));
+    view_favorite.append('gender_interested', localStorage.getItem('genderIntersetedIn'));
+
+    const view_favorite_url = `${homeBaseURL}favorite/view_favorite`;
+    const response_view_favorite = await postAPI(view_favorite_url,view_favorite);
+
+    for(let i=0; i<response_view_favorite.data.data.length; i++){
+        favoriteContainer.innerHTML +=`
+            <div class="person">
+                <div class="person-column1">
+                    <div class="person-image">
+                        <img src="../dating-server/dating-server/public/${response_view_people.data.data[i].picture}" alt="person">
+                    </div>
+                    <div class="person-information">
+                        <p>${response_view_people.data.data[i].name}</p>
+                        <p>${response_view_people.data.data[i].age}</p>
+                        <p>${response_view_people.data.data[i].location}</p>
+                    </div>
+                    <div class="person-option">
+                        <i class="like fa fa-heart" data-value=${response_view_people.data.data[i].id}></i>
+                    </div>
+                </div>
+                <div class="person-column2">
+                    <div class="person-interest">
+                        <h1>Interests:</h1>
+                        <ul>
+                            <li>${response_view_people.data.data[i].interest}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+
+    // Accessing each favorite button in the home page
     const likes = document.querySelectorAll('.like')
-    console.log(likes)
     likes.forEach(like => {
         like.addEventListener('click', async()=>{
             
